d9c6c774357b55b5801f99e1a3be858276850fb4
devalenya1 <127754942+devalenya1@users.noreply.github.com>
2023-08-05 15:57:29 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/kayan/teacher/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kayan.teacher
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,22 @@
+//database urls
+//Please add your admin panel url here and make sure you do not add '/' at the end of the url
+import 'package:eschool_teacher/utils/labelKeys.dart';
+
+const String baseUrl = "http://kayanschool.atwebpages.com/management/public";
+
+const String databaseUrl = "$baseUrl/api/";
+
+//error message display duration
+const Duration errorMessageDisplayDuration = Duration(milliseconds: 3000);
+
+String getExamStatusTypeKey(String examStatus) {
+  if (examStatus == "0") {
+    return upComingKey;
+  }
+  if (examStatus == "1") {
+    return onGoingKey;
+  }
+  return completedKey;
+}
+
+List<String> examFilters = [allExamsKey, upComingKey, onGoingKey, completedKey];
